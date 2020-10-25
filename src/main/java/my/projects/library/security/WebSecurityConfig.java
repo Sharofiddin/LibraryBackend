ca3130c865afc7aaed2ca4036bc9ac65383541ba
sharofiddin <pardayev.sharofiddin@gmail.com>
2020-10-25 14:53:19 +0500

    ref

--- src/main/java/my/projects/library/security/WebSecurityConfig.java
+++ src/main/java/my/projects/library/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package my.projects.library.security;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
